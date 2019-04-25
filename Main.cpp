4043816e7e0e21e46033d9fc850959f8045f10f8
BREINERBATALLA <47467079+BREINERBATALLA@users.noreply.github.com>
2019-04-24 21:44:52 -0500

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <stdlib.h>
+#include "Menu.h"
+
+using namespace std ;
+
+
+int main () {
+	
+	Menu inicio;
+	
+	inicio.MenuGeneral(); 
+	
+	
+	
+	system("pause") ;
+	return 0 ;
+}
