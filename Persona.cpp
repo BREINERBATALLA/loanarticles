95be907d3564f30d4648f659282c0766ad8c1a08
BREINERBATALLA <47467079+BREINERBATALLA@users.noreply.github.com>
2019-04-24 21:47:26 -0500

    Create Persona.cpp

---
+++ Persona.cpp
@@ -0,0 +1,22 @@
+
+#include "Persona.h"
+
+
+Persona::Persona() {
+}
+
+
+
+
+Persona::Persona(string nombre, int edad, string apellido) {
+	this->nombre = nombre;
+	this->edad = edad;
+	this->apellido = apellido;
+}
+
+
+
+
+
+Persona::~Persona() {
+}
